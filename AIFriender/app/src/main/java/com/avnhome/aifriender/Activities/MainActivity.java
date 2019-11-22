b0730acd1841fa9a19f64c4ce420daa2522628c5
MINHTIENUIT <vanminhtien96@gmail.com>
2019-11-22 17:47:43 +0700

    Edit FragmentUser and FragmentChart Description

--- AIFriender/app/src/main/java/com/avnhome/aifriender/Activities/MainActivity.java
+++ AIFriender/app/src/main/java/com/avnhome/aifriender/Activities/MainActivity.java
@@ -3,33 +3,44 @@ package com.avnhome.aifriender.Activities;
 import android.content.Intent;
 import android.os.Bundle;
 
+import com.avnhome.aifriender.Fragments.ChartFragment;
+import com.avnhome.aifriender.Fragments.DescriptionChartFragment;
+import com.avnhome.aifriender.Fragments.DescriptionUserFragment;
 import com.avnhome.aifriender.R;
 import com.avnhome.aifriender.Twitter.TwitterManager;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.sothree.slidinguppanel.SlidingUpPanelLayout;
 import com.twitter.sdk.android.core.SessionManager;
 import com.twitter.sdk.android.core.TwitterCore;
 import com.twitter.sdk.android.core.TwitterSession;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.work.WorkManager;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.Toast;
 
-import java.util.Timer;
-import java.util.TimerTask;
-
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SlidingUpPanelLayout.PanelSlideListener, View.OnClickListener {
     private Button logoutBtn;
     private FirebaseAuth auth;
-    SessionManager<TwitterSession> sessionManager;
-    TwitterSession twitterSession;
+    private SessionManager<TwitterSession> sessionManager;
+    private TwitterSession twitterSession;
 
-    private final WorkManager workManager = WorkManager.getInstance();
+
+    private SlidingUpPanelLayout mLayout;
+
+    private ChartFragment chartFragment;
+    private DescriptionChartFragment descriptionChartFragment;
+    private DescriptionUserFragment descriptionUserFragment;
+
+
+//    private final WorkManager workManager = WorkManager.getInstance();
 
     private int backButtonCount = 0;
 
@@ -48,7 +59,15 @@ public class MainActivity extends AppCompatActivity {
         twitterSession = sessionManager.getActiveSession();
         auth = FirebaseAuth.getInstance();
 
+        chartFragment = ChartFragment.newInstance("Test1", "Test2");;
+        descriptionChartFragment = DescriptionChartFragment.newInstance("Test1", "Test2");
+        descriptionUserFragment = DescriptionUserFragment.newInstance("Test1", "Test2");
+        loadChartFragment();
+
         findViewByIds();
+
+        mLayout.addPanelSlideListener(this);
+        mLayout.setFadeOnClickListener(this);
 
         logoutBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -62,6 +81,7 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
 
 //        FloatingActionButton fab = findViewById(R.id.fab);
 //        fab.setOnClickListener(new View.OnClickListener() {
@@ -97,6 +117,7 @@ public class MainActivity extends AppCompatActivity {
 
     private void findViewByIds(){
         logoutBtn = findViewById(R.id.logout_btn);
+        mLayout = findViewById(R.id.sliding_layout);
     }
 
     private void updateLoginUI(){
@@ -130,5 +151,32 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(this, "Press the back button once again to close the application.", Toast.LENGTH_SHORT).show();
             backButtonCount++;
         }
+    }
+
+    public void loadChartFragment(){
+        chartFragment = ChartFragment.newInstance("Test1", "Test2");
+        descriptionChartFragment = DescriptionChartFragment.newInstance("Test1", "Test2");
+        descriptionUserFragment = DescriptionUserFragment.newInstance("Test1", "Test2");
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.add(R.id.content_chart_frg, chartFragment);
+        fragmentTransaction.add(R.id.content_description_chart, descriptionChartFragment);
+        fragmentTransaction.add(R.id.content_description_User, descriptionUserFragment);
+        fragmentTransaction.commit();
+    }
+
+    @Override
+    public void onClick(View view) {
+        mLayout.setPanelState(SlidingUpPanelLayout.PanelState.COLLAPSED);
+    }
+
+    @Override
+    public void onPanelSlide(View panel, float slideOffset) {
+        Log.i("TIEN", "onPanelSlide, offset " + slideOffset);
+    }
+
+    @Override
+    public void onPanelStateChanged(View panel, SlidingUpPanelLayout.PanelState previousState, SlidingUpPanelLayout.PanelState newState) {
+        Log.i("TIEN", "onPanelStateChanged " + newState);
     }
 }
